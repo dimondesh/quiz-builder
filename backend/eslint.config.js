a200ce658539a05e21a56f146a4d6b58dabf7c56
dimondesh <dimondesh1337@gmail.com>
2025-11-18 15:35:35 +0200

    backend fix

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,16 +1,25 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
+const js = require("@eslint/js");
+const ts = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier,
-  {
-    files: ["src/**/*.{ts,js}"],
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
+module.exports = ts.config({
+  files: ["src/**/*.{ts,js}"],
+  languageOptions: {
+    parser: ts.parser,
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+    globals: {
+      console: "readonly",
+      process: "readonly",
     },
   },
-];
+  ignores: ["dist/**"],
+
+  extends: [js.configs.recommended, ...ts.configs.recommended, prettier],
+
+  rules: {
+    "no-unused-vars": "warn",
+    "no-undef": "off",
+  },
+});
